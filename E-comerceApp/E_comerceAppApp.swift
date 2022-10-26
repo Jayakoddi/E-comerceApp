bb20599cc71346d930efaa7e7d38eac950088840
Jayakoddi <priyanathan.christy@gmail.com>
2022-10-26 10:22:49 +0200

    Initial Commit

---
+++ E-comerceApp/E_comerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_comerceAppApp.swift
+//  E-comerceApp
+//
+//  Created by Christy Dinakaran on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct E_comerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
